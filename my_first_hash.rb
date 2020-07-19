24e85c6e72cef995537eae3ed6624d18931b3881
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-19 15:46:01 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,8 +22,6 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -33,8 +31,9 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
-
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+  shipping_manifest
 
   # add 4 gun powder to the shipping_manifest hash below
 
